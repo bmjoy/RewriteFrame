c17ec2b3e2bb50f3eef7f7538ed84af2aaa7aa72
fqkw6 <632636416@qq.com>
2020-07-18 15:58:24 +0800

    提交

---
+++ tm/Scripts/NGUILesson/NGUIL01.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NGUIL01 : MonoBehaviour
+{
+    public UIButton MyUIButton;
+    public Button button;
+    void Start()
+    {
+       // button.onClick.AddListener(ONClick);
+        UIEventListener.Get(MyUIButton.gameObject).onClick = OnCL;
+    }
+
+    public void ONClick()
+    {
+        Debug.LogError("dayin");
+    }
+    public void OnCL(GameObject go)
+    {
+        Debug.LogError("dayin");
+    }
+}
